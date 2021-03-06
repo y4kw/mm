4989d6429c2c5db47682c820bf859c0692ab4c63
y4kw <y4kw@math.sci.hokudai.ac.jp>
2021-03-06 13:55:40 +0900

    erase comments

--- app/src/main/java/com/example/mm/MainActivity.java
+++ app/src/main/java/com/example/mm/MainActivity.java
@@ -6,22 +6,16 @@ import android.graphics.Bitmap;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.SystemClock;
-import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View;
 import android.webkit.CookieManager;
 import android.webkit.CookieSyncManager;
 import android.webkit.WebResourceResponse;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-//import android.support.design.widget.FloatingActionButton;
 
-import androidx.annotation.RequiresApi;
-
-
-//public class MainActivity extends AppCompatActivity {
 public class MainActivity extends Activity implements OnClickListener{
 
     public void d(String... message) {
@@ -41,10 +35,8 @@ public class MainActivity extends Activity implements OnClickListener{
     private WebView webview;
 
     public static int reloaded = 0;
-    //public int reloadmax = 5;
     public final int reloadmax = -4;
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         d();
@@ -69,10 +61,6 @@ public class MainActivity extends Activity implements OnClickListener{
             cookieSyncMngr.sync();
         }
 
-        //CookieManager.getInstance().removeAllCookies(null);
-        //CookieManager.getInstance().flush();
-
-        //WebView webview = (WebView) findViewById(R.id.webView1);
         webview = (WebView) findViewById(R.id.webView1);
 
         //if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB){
@@ -81,38 +69,24 @@ public class MainActivity extends Activity implements OnClickListener{
         //    reloadmax = 5;
         //}
 
-        //webview.requestFocus();
-        webview.getSettings().setLightTouchEnabled(true);
         webview.getSettings().setJavaScriptEnabled(true);
         webview.getSettings().setBuiltInZoomControls(true);
         webview.getSettings().setDisplayZoomControls(false);
         webview.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
         webview.getSettings().setSupportZoom(true);
-        ////good!
         webview.setVerticalScrollbarOverlay(true);
-        //webview.setInitialScale(110);
         webview.setInitialScale(300);
         //webview.getSettings().setUseWideViewPort(true);
         //webview.getSettings().setLoadWithOverviewMode(true);
         //
-        webview.setVerticalScrollBarEnabled(true);
-        webview.setHorizontalScrollBarEnabled(true);
-
-
-
-        //webview.setInitialScale(1);
-        //webview.getSettings().setLoadWithOverviewMode(true);
-        //webview.getSettings().setUseWideViewPort(true);
+        //webview.setVerticalScrollBarEnabled(true);
+        //webview.setHorizontalScrollBarEnabled(true);
         //webview.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
         d();
         webview.loadUrl(url);
 
-        //Button button = findViewById(R.id.button);
-
         ProgressDialog loading = new ProgressDialog(this);
-        //loading.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
         loading.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-        //loading.setMessage("onPageStarted");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(this);
@@ -152,14 +126,9 @@ public class MainActivity extends Activity implements OnClickListener{
                 //SystemClock.sleep(1000);
                 if (reloaded < reloadmax) {
                     if (checkOnPageStartedCalled == true) {
-                        //webview.reload();
                         reloaded++;
-                        //SystemClock.sleep(1000*reloaded);
-                        //webview.reload();
-                        //webview.loadUrl(url);
                         webview.loadUrl("javascript:window.location.reload( true )");
                         d("checkOnPageStartedCalled==true reloaded=" + String.valueOf(reloaded));
-                        //checkOnPageStartedCalled = false;
                     } else {
                         showPdfFile(url);
                         d("NOTcheckOnPageStartedCalled==true");
@@ -174,7 +143,6 @@ public class MainActivity extends Activity implements OnClickListener{
                 return null;
             }
 
-
             @Override
             public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
                 super.onReceivedError(view, errorCode, description, failingUrl);
@@ -185,54 +153,29 @@ public class MainActivity extends Activity implements OnClickListener{
 
     }
 
-    //private LinearLayout.LayoutParams createParam(int w, int h){
-    //    return new LinearLayout.LayoutParams(w, h);
-    //}
-
     public void onClick(View v) {
+        d();
         if (v.getId() == R.id.fab) {
             webview.clearCache(false);
+            SystemClock.sleep(1000);
             webview.loadUrl(url);
             SystemClock.sleep(1000);
-            d();
         }
         if (v.getId() == R.id.fab1) {
-            //webview.clearCache(false);
+            webview.clearCache(false);
             webview.loadUrl(url1);
             SystemClock.sleep(1000);
-            d();
-            //webview.loadUrl("javascript:window.location.reload( true )");
-            //webview.loadUrl("javascript:document.elementFromPoint(500,1206).click()");
         }
     }
 
     public void onRestart() {
-        //webview.clearCache(true);
         super.onRestart();
-        //webview.loadUrl(url);
-        //webview.loadUrl("javascript:window.location.reload( true )");
         d();
     }
 
     @Override
     public void onResume() {
-        //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-        //    CookieManager.getInstance().removeAllCookies(null);
-        //    CookieManager.getInstance().flush();
-        //} else {
-        //    CookieSyncManager cookieSyncMngr=CookieSyncManager.createInstance(getApplicationContext());
-        //    cookieSyncMngr.startSync();
-        //    CookieManager cookieManager=CookieManager.getInstance();
-        //    cookieManager.removeAllCookie();
-        //    cookieManager.removeSessionCookie();
-        //    cookieSyncMngr.stopSync();
-        //    cookieSyncMngr.sync();
-        //}
-
         super.onResume();
-        //webview.loadUrl(url);
-        //webview.clearCache(true);
-        //webview.loadUrl("javascript:window.location.reload( true )");
         d();
     }
 
