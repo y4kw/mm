7ab150db0aae637b783627b55176c98d35fa315a
y4kw <y4kw@math.sci.hokudai.ac.jp>
2021-03-09 15:59:23 +0900

    toolbar fixed  version

--- app/src/main/java/com/example/mm/MainActivity.java
+++ app/src/main/java/com/example/mm/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
                 Thread.currentThread().getStackTrace()[3].getLineNumber()) + " "
                 + Thread.currentThread().getStackTrace()[3].getMethodName() + " " + str;
         android.util.Log.d("MYDEBUG", msg);
-        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
